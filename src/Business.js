a18c72737fb52fb69d7cbcce3a48e7b1534005ea
leroy brown <leroybrown247@gmail.com>
2023-11-09 09:53:04 +1300

    Ravenous: Set up a Business component

---
+++ src/Business.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Create a Business component. The component will represent how a business (a restaurant) in Ravenous will be formatted and styled.
+
+// Each Business component should display an image, name, address, city, state, zipcode, category, rating, and review count.
+
+// Create the Business Component Function
+
+const Business = () => {
+    return (
+        <div> 
+
+        </div>
+    )
+}
+
+export default Business;
