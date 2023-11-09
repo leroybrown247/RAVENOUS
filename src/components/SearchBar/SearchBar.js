9f911fcae4fe15c266ca344759c5c449a4b0f87f
leroy brown <leroybrown247@gmail.com>
2023-11-10 09:22:23 +1300

    Ravenous: Setting up css modules for components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-// Create a SearchBar component that users can use to search for businesses on the Yelp platform.
-
-// The search bar should contain two inputs, one for the user’s search terms and the selected location.
-
-// The search bar should also offer sorting options such as Best Match, Highest Rated, and Most Reviewed.
-
 const sortingOptions = {
   "Best Match": "best_match",
   "Highest Rated": "rating",
