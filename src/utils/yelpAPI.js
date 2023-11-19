fe28807539f6e264b43abb6aa957fff6ab4828f7
leroy brown <leroybrown247@gmail.com>
2023-11-19 23:11:46 +1300

    Ravenous: Define constatnts for API

--- src/utils/yelpAPI.js
+++ src/utils/yelpAPI.js
@@ -1,2 +1,7 @@
-const apiKey = "-CJPQb3Nh13vVk4NP4JFsc58OzunliBDcmn9TIPls8oqO5yctmCV4a1UMdFUrl1OTa5qaHDjG9BG0oiPnUuYzUqYOCfqzT_PNrqXhSd9ROBkZbGZEMc_vnegk9dZZXYx"
+// Step 1: Import necessary dependencies
+import 'whatwg-fetch';
+
+// Step 2: Define constants for the Yelp API
+const apiKey = '-CJPQb3Nh13vVk4NP4JFsc58OzunliBDcmn9TIPls8oqO5yctmCV4a1UMdFUrl1OTa5qaHDjG9BG0oiPnUuYzUqYOCfqzT_PNrqXhSd9ROBkZbGZEMc_vnegk9dZZXYx'
+const  apiUrl = 'https://api.yelp.com/v3/businesses/search'
 
