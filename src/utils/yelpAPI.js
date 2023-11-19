92d439d39ab943d395b9e58e30e1a183d6fa43ee
leroy brown <leroybrown247@gmail.com>
2023-11-20 10:53:57 +1300

    Ravenous: Construct the URL with search parameters

--- src/utils/yelpAPI.js
+++ src/utils/yelpAPI.js
@@ -7,6 +7,7 @@ const  apiUrl = 'https://api.yelp.com/v3/businesses/search'
 
 // Step 3: Create a function to interact with the Yelp API
 const search = async (terms, location, sortBy) => {
-    
+    // Step 4: Construct the URL with search parameters
+    const url = `${apiUrl}?term=${terms}&location=${location}&sortBy=${sortBy}`
 }
 
