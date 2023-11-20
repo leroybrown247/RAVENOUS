79d49078d6f03a2420bf58efd6247abc459c3fc6
leroy brown <leroybrown247@gmail.com>
2023-11-20 21:57:39 +1300

    Ravenous: Added onKeyPress to input fields

--- src/utils/yelpAPI.js
+++ src/utils/yelpAPI.js
@@ -1,7 +1,7 @@
 import "whatwg-fetch";
 
 const apiKey = process.env.REACT_APP_YELP_API_KEY;
-  const apiUrl = "https://api.yelp.com/v3/businesses/search";
+const apiUrl = "https://api.yelp.com/v3/businesses/search";
 
 const search = async (terms, location, sortBy) => {
   const url = `https://cors-anywhere.herokuapp.com/${apiUrl}?term=${terms}&location=${location}&sortBy=${sortBy}`;
@@ -12,7 +12,7 @@ const search = async (terms, location, sortBy) => {
       headers: {
         Authorization: `Bearer ${apiKey}`,
       },
-      redirect: 'follow', // Handle redirects
+      redirect: "follow",
     });
 
     if (response.ok) {
@@ -20,22 +20,25 @@ const search = async (terms, location, sortBy) => {
 
       console.log(jsonResponse);
 
-      const businesses = jsonResponse.businesses && Array.isArray(jsonResponse.businesses)
-      ? jsonResponse.businesses.map((business) => {
-          return {
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.location.address1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories.map((category) => category.title).join(', '), 
-            rating: business.rating,
-            reviewCount: business.review_count,
-          };
-        })
-      : [];
+      const businesses =
+        jsonResponse.businesses && Array.isArray(jsonResponse.businesses)
+          ? jsonResponse.businesses.map((business) => {
+              return {
+                id: business.id,
+                imageSrc: business.image_url,
+                name: business.name,
+                address: business.location.address1,
+                city: business.location.city,
+                state: business.location.state,
+                zipCode: business.location.zip_code,
+                category: business.categories
+                  .map((category) => category.title)
+                  .join(", "),
+                rating: business.rating,
+                reviewCount: business.review_count,
+              };
+            })
+          : [];
 
       return businesses;
     } else {
